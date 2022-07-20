6d03a120737fc9edab5ae7e06c5225c4bc404776
Manisha-Salve <95564866+Manisha-Salve@users.noreply.github.com>
2022-07-19 23:48:22 -0400

    c program
    write a first demo program ?

---
+++ Demo.c
@@ -0,0 +1,12 @@
+// It is used to include the header file
+#include<stdio.h>
+
+// Entry point function form where our application starts its execution
+int main()
+{
+    // It is used to display on screen
+    printf("Jay Ganesh..\n");
+    
+    // Return the status to the operating system
+    return 0;
+}
